4e6ca5f9d78b1b899a775a58456dc12793f5fdf1
Chris Neale <chris@komododigital.co.uk>
2019-01-24 11:10:29 +0000

    Colors and speeds

--- src/Grid1.jsx
+++ src/Grid1.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import canvasSketch from "canvas-sketch";
 
+const colors = ["#B4D3AA", "#84D2DE", "#EA8F3C", "#B24629"];
+
 const settings = {
   dimensions: [600, 600],
   animate: true,
@@ -16,32 +18,53 @@ export default class Sketch5 extends React.Component {
   }
   componentDidMount() {
     const sketch = () => {
-      const cells = 10;
+      const cells = 6;
+      const margin = 5;
+      const lineWidth = 4;
+
+      let r = 0;
+
       return ({ context, width, height, frame }) => {
         const xPos = width / cells;
         const yPos = height / cells;
 
-        context.fillStyle = "rgba(255,255,255,0.5)";
+        context.fillStyle = "#FFFFFF";
         context.fillRect(0, 0, width, height);
-
-        context.strokeStyle = "rgb(0,0,0,0.5)";
 
         for (let i = 0; i < cells * cells; i++) {
           const x = i % cells;
           const y = Math.floor(i / cells);
 
+          // if (Math.floor(frame / (60 / 3)) % (60 / 3) === 0) {
+          //   r = frame;
+          // }
+
           context.beginPath();
           context.translate(x * xPos + xPos * 0.5, y * yPos + yPos * 0.5);
-          context.rotate((frame * Math.PI) / 180);
-          context.rect(xPos * -0.5, yPos * -0.5, xPos, yPos);
-          context.rotate(((360 - frame * 2) * Math.PI) / 180);
+          context.rotate((frame * 0.5 * Math.PI) / 180);
+          context.rect(
+            (xPos - margin) * -0.5,
+            (yPos - margin) * -0.5,
+            xPos - margin,
+            yPos - margin
+          );
+          context.strokeStyle = colors[0];
+          context.lineWidth = lineWidth;
+          context.stroke();
+          context.resetTransform();
+
+          context.beginPath();
+          let offset = x * y * 10;
+          context.translate(x * xPos + xPos * 0.5, y * yPos + yPos * 0.5);
+          context.rotate((offset + (360 - frame * 2) * Math.PI) / 180);
           context.rect(
             xPos * -0.4 + xPos * 0.2,
             yPos * -0.4 + yPos * 0.2,
             xPos * 0.4,
             yPos * 0.4
           );
-          context.stroke();
+          context.fillStyle = colors[1];
+          context.fill();
           context.resetTransform();
         }
       };
